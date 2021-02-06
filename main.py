bfad56a7d2439b159a44c35fa37ad4645866ac63
stepollo2 <56636447+stepollo2@users.noreply.github.com>
2021-02-06 16:16:29 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+from boltons import iterutils
+import csv
+
+site= "https://etherscan.io/token/generic-tokenholders2?a=0x622f2962ae78e8686ecc1e30cf2f9a6e5ac35626&s=0&p="
+hdr = {'User-Agent': 'Mozilla/5.0'}
+req = Request(site,headers=hdr)
+page = urlopen(req)
+tablerows = []
+
+pagen = 0
+while pagen <= 10000:
+    pagen += 1
+    site= "https://etherscan.io/token/generic-tokenholders2?a=0x622f2962ae78e8686ecc1e30cf2f9a6e5ac35626&s=0&p="+str(pagen)
+    hdr = {'User-Agent': 'Mozilla/5.0'}
+    req = Request(site,headers=hdr)
+    page = urlopen(req)
+    soup = BeautifulSoup(page)
+    table = soup.findAll("table")
+    rows = table[0].findChildren(['th', 'tr'])
+    for row in rows:
+        cells = row.findChildren('td')
+        for cell in cells:
+            rows_2 = []
+            value = cell.string
+            if value == 'There are no matching entries':
+                break
+            else:
+                tablerows.append(value)
+
+newlist = iterutils.chunked(tablerows, 6)
+print(newlist)
+
+with open('file.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerows(newlist)
